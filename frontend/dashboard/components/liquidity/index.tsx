25263001e9c53f39512d7c89ac3445c800b487f7
OCOS UK <support@ocos.io>
2025-07-18 16:22:51 +0400

    index.tsx

---
+++ frontend/dashboard/components/liquidity/index.tsx
@@ -0,0 +1,21 @@
+// Shared global components
+export * from "./FeeInfo";
+export * from "./OraclePricePanel";
+export * from "./BridgePanel";
+export * from "./GovernancePanel";
+export * from "./PoolChart";
+export * from "./PoolStatusTag";
+export * from "./PoolTokensBadge";
+export * from "./PoolStatsPanel";
+export * from "./PoolActivityFeed";
+export * from "./PoolRewardsPanel";
+export * from "./PoolPositionsTable";
+export * from "./PoolDepositForm";
+export * from "./PoolWithdrawForm";
+export * from "./PoolSwapForm";
+
+// DAO dashboard components
+export * from "./dao";
+
+// Liquidity dashboard components
+export * from "./liquidity";
