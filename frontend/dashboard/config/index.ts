4678ac27de38bb90c5a9360d3747fa46d2bb0b05
OCOS UK <support@ocos.io>
2025-07-22 15:05:10 +0400

    index.ts

---
+++ frontend/dashboard/config/index.ts
@@ -0,0 +1,7 @@
+export * from "./app.config";
+export * from "./routes.config";
+export * from "./api.config";
+export * from "./theme.config";
+export * from "./featureFlags.config";
+export * from "./languages.config";
+export * from "./env.config";
